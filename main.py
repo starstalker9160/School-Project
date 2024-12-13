3a57cbad9af64d6a6f81f28e337a0935fc71559a
star.stalker9160 <abyazjschool@gmail.com>
2024-12-13 11:16:10 +0400

    basic1

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-def open_browser():
-    webbrowser.open('http://127.0.0.1:8080')
+@app.route('/split')
+def split():
+    return render_template('split.html')
+
+@app.route('/merge')
+def merge():
+    return render_template('merge.html')
 
 if __name__ == '__main__':
-    # threading.Timer(1, open_browser).start()
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    try:
+        print("[  OK  ] Starting app")
+        threading.Timer(1, lambda: webbrowser.open('http://127.0.0.1:8080')).start()
+        app.run(host='127.0.0.1', port=8080, debug=True)
+        print("[  OK  ] App started")
+    except Exception as e:
+        print(f"[ FAIL ] Error: {e}")
