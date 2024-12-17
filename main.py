c9b845a523f4634f64b263b47b74dc1ef13e89fc
prokenz101 <kenz.v@outlook.com>
2024-12-17 12:41:57 +0400

    refactor

--- main.py
+++ main.py
@@ -4,82 +4,93 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = "uploads"
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 color = random.choice(["#EE6352", "#746AFE", "#59CD90", "#3FA7D6", "#FAC05E"])
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html', bg = color)
+    return render_template("index.html", bg=color)
 
-@app.route('/split')
+
+@app.route("/split")
 def split():
-    return render_template('split.html', bg = color)
+    return render_template("split.html", bg=color)
 
-@app.route('/merge')
+
+@app.route("/merge")
 def merge():
-    return render_template('merge.html', bg = color)
+    return render_template("merge.html", bg=color)
 
-@app.route('/from-docx')
+
+@app.route("/from-docx")
 def from_docx():
-    return render_template('from-docx.html', bg = color)
+    return render_template("from-docx.html", bg=color)
 
-@app.route('/to-docx')
+
+@app.route("/to-docx")
 def to_docx():
-    return render_template('to-docx.html', bg = color)
+    return render_template("to-docx.html", bg=color)
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    
-    file = request.files['file']
-    metadata = request.form.get('metadata')
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+    metadata = request.form.get("metadata")
 
     if not metadata:
-        return jsonify({'error': 'No metadata provided'}), 400
+        return jsonify({"error": "No metadata provided"}), 400
 
     try:
         metadata = json.loads(metadata)
     except ValueError:
-        return jsonify({'error': 'invalid metadata format'}), 400
+        return jsonify({"error": "invalid metadata format"}), 400
 
-    if file.filename == '':
-        return jsonify({'error': 'no selected file'}), 400
+    if file.filename == "":
+        return jsonify({"error": "no selected file"}), 400
 
-    if file and file.filename.endswith('.pdf'):
-        pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    if file and file.filename.endswith(".pdf"):
+        pdf_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
         file.save(pdf_path)
 
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], "metadata.json"), 'w') as f:
+        with open(os.path.join(app.config["UPLOAD_FOLDER"], "metadata.json"), "w") as f:
             json.dump(metadata, f, indent=4)
 
         Handler.handle()
 
-        return jsonify({'message': 'upload completed successfully'}), 200
+        return jsonify({"message": "upload completed successfully"}), 200
     else:
-        return jsonify({'error': 'invalid file type'}), 400
+        return jsonify({"error": "invalid file type"}), 400
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html', bg = color), 404
+    return render_template("404.html", bg=color), 404
+
+
 @app.errorhandler(405)
 def not_found(error):
-    return render_template('405.html', bg = color), 405
+    return render_template("405.html", bg=color), 405
+
 
 def download(filename, directory):
     """Use to send file to the user for download"""
     return send_from_directory(directory, filename, as_attachment=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         print("[  OK  ] Starting app")
-        # threading.Timer(1, lambda: webbrowser.open('http://127.0.0.1:8080')).start()
-        app.run(host='127.0.0.1', port=8080, debug=True)
+        threading.Timer(1, lambda: webbrowser.open('http://127.0.0.1:8080')).start()
+        app.run(host="127.0.0.1", port=8080, debug=True)
         print("[  OK  ] App started")
     except Exception as e:
         print(f"[ FAIL ] Error: {e}")
