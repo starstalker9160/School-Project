fae465ac5341e02e45c8357cc91836c5d299829f
star.stalker9160 <abyazjschool@gmail.com>
2024-12-18 11:42:53 +0400

    removed a thingy

--- main.py
+++ main.py
@@ -46,7 +46,6 @@ def upload_file():
         return jsonify({"error": "No metadata provided"}), 400
 
     try:
-        print(metadata)
         metadata = json.loads(metadata)
     except ValueError:
         return jsonify({"error": "invalid metadata format"}), 400
@@ -84,7 +83,7 @@ def not_found_405(error):
 if __name__ == "__main__":
     try:
         print("[  OK  ] Starting app")
-        threading.Timer(1, lambda: webbrowser.open('http://127.0.0.1:8080')).start()
+        # threading.Timer(1, lambda: webbrowser.open('http://127.0.0.1:8080')).start()
         app.run(host="127.0.0.1", port=8080, debug=True)
     except Exception as e:
         print(f"[ FAIL ] Error: {e}")
