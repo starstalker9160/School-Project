84e8c788bf4939ee55ab48663f7c21a545c626b5
prokenz101 <kenz.v@outlook.com>
2025-01-29 17:13:56 +0400

    updated name

--- src/Handler.py
+++ src/Handler.py
@@ -8,33 +8,35 @@ def handle():
         f.close()
     match metadata["operation"]:
         case "split":
-            PDFThingy.split(metadata["file name"], metadata["operationSpecificInfo"]["splitOnPage"])
+            PDFHandler.split(metadata["file name"], metadata["operationSpecificInfo"]["splitOnPage"])
         case "to-docx":
-            PDFThingy.to_docx(metadata["file name"])
+            PDFHandler.to_docx(metadata["file name"])
         case "from-docx":
-            PDFThingy.from_docx(metadata["file name"])
+            PDFHandler.from_docx(metadata["file name"])
     
 
-class PDFThingy:
+class PDFHandler:
     @staticmethod
     def split(fileName: str, page: int) -> None:
         # relevant functionality
-        PDFThingy.cleanup()
+        
+
+        PDFHandler.cleanup()
     
     @staticmethod
     def merge(file1: str, file2: str, order: list) -> None:
         # relevant functionality
-        PDFThingy.cleanup()
+        PDFHandler.cleanup()
     
     @staticmethod
     def to_docx(fileName: str) -> None:
         # relevant functionality
-        PDFThingy.cleanup()
+        PDFHandler.cleanup()
 
     @staticmethod
     def from_docx(fileName: str) -> None:
         # relevant functionality
-        PDFThingy.cleanup()
+        PDFHandler.cleanup()
     
     @staticmethod
     def cleanup() -> None:
