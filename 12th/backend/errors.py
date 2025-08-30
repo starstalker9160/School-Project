7c1b11562935d9e66e1f0646532bcdfd114ca21f
star.stalker9160 <abyazjschool@gmail.com>
2025-08-30 18:39:57 +0400

    removed 12th

--- 12th/backend/errors.py
+++
@@ -1,9 +0,0 @@
-class ScanError(Exception):
-    """Raised when there is an error in scanning"""
-
-    def __init__(self, message="ScanErr", *args: object) -> None:
-        super().__init__(*args)
-        self.message = message
-
-    def __str__(self) -> str:
-        return self.message
